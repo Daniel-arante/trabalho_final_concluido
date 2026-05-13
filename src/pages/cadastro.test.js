757dc1112eedf6abcf30f5053bd2e0e579d8bebf
Daniel Arantes <daniellopes6707@exemplo.com>
2026-05-13 12:54:58 -0300

    Correção dos testes e caminhos de import

--- src/pages/cadastro.test.js
+++ src/pages/cadastro.test.js
@@ -1,4 +1,4 @@
 const testedocadastro = require('./function.app');
 test('teste do cadastro ', () => {
-    expect(testedocadastro.testedocadastro('pedrodaniel@gmail.com', 'Daniel@2')).toBe(true);
+    expect(testedocadastro.testedocadastro('pedrodaniel@gmail.com ', 'Daniel@2')).toBe(true);
 });
